83350a4d770fc3d6f6ce8d1e27794ce4e0b78d60
Mike Pallas <mikeycp@gmail.com>
2014-08-17 18:34:38 -0700

    Fixed pin deletion error

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,53 +1,40 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
-  # GET /pins
-  # GET /pins.json
   def index
     @pins = Pin.all
   end
 
-  # GET /pins/1
-  # GET /pins/1.json
   def show
   end
 
-  # GET /pins/new
   def new
     @pin = Pin.new
   end
 
-  # GET /pins/1/edit
   def edit
   end
 
-  # POST /pins
-  # POST /pins.json
   def create
     @pin = Pin.new(pin_params)
-      if @pin.save
-        redirect_to @pin, notice: 'Pin was successfully created.'
-      else
-        render :new
-      end
-  end
-
-  # PATCH/PUT /pins/1
-  # PATCH/PUT /pins/1.json
-  def update
-      if @pin.update(pin_params)
-        redirect_to @pin, notice: 'Pin was successfully updated.'
-         else
-        render :edit
-         end
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
-  # DELETE /pins/1
-  # DELETE /pins/1.json
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
   def destroy
     @pin.destroy
-      redirect_to pins_url, notice: 'Pin was successfully destroyed.'
+    redirect_to pins_url
   end
 
   private
@@ -60,3 +47,4 @@ class PinsController < ApplicationController
     def pin_params
       params.require(:pin).permit(:description)
     end
+end
